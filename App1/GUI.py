e4aa661e3e608838d2f01489eb2955af70236904
Avinash <avkr258@gmail.com>
2024-01-29 15:26:56 -0500

    Initial Commit

---
+++ App1/GUI.py
@@ -0,0 +1,13 @@
+import functions
+import PySimpleGUI as zx
+
+label = zx.Text("Type in your Workouts")
+input_box = zx.InputText(tooltip="Enter an exercise")
+add_button = zx.Button("Add")
+
+window = zx.Window('My Workout Planner App', layout=[[label], [input_box, add_button]])
+window.read()
+window.close()
+
+
+
