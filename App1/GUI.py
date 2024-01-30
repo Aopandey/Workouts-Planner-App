f876fa1818dc3829a6a4f1996079d33603065f5c
Avinash <avkr258@gmail.com>
2024-01-30 11:31:27 -0500

    Changed the theme, added clock, and handled edit and complete error

--- App1/GUI.py
+++ App1/GUI.py
@@ -1,28 +1,31 @@
 import functions
 import PySimpleGUI as zx
+import time
 
+zx.theme('DarkBlack')
+
+clock = zx.Text('',key='clock')
 label = zx.Text("Type in your Workouts")
 input_box = zx.InputText(tooltip="Enter an exercise",
                          key="Exercise")
 add_button = zx.Button("Add")
 list_box = zx.Listbox(values=functions.get_workouts(), key='Exercises',
-                      enable_events=True, size=[38, 8])
+                      enable_events=True, size=[38, 10])
 edit_button = zx.Button("Edit")
 complete_button = zx.Button("Complete")
 exit_button = zx.Button("Exit")
 
 window = zx.Window('My Workout Planner App',
-                   layout=[[label],
+                   layout=[[clock],
+                           [label],
                            [input_box, add_button],
                            [list_box, edit_button, complete_button],
                            [exit_button]],
-                   font=('Times new roman', 12))
+                   font=('Helvetica', 12))
 
 while True:
-    event, values = window.read()
-    print(1, event)
-    print(2, values)
-    print(3, values['Exercises'])
+    event, values = window.read(timeout=250)
+    window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case "Add":
             workouts = functions.get_workouts()
@@ -31,21 +34,27 @@ while True:
             functions.write_workouts(workouts)
             window['Exercises'].update(values=workouts)
         case "Edit":
-            Exercise_to_edit = values['Exercises'][0]
-            new_exercises = values['Exercise']
+            try:
+                Exercise_to_edit = values['Exercises'][0]
+                new_exercises = values['Exercise']
 
-            workouts = functions.get_workouts()
-            index = workouts.index(Exercise_to_edit)
-            workouts[index] = new_exercises
-            functions.write_workouts(workouts)
-            window['Exercises'].update(values=workouts)
+                workouts = functions.get_workouts()
+                index = workouts.index(Exercise_to_edit)
+                workouts[index] = new_exercises
+                functions.write_workouts(workouts)
+                window['Exercises'].update(values=workouts)
+            except IndexError:
+                zx.popup("Please select an exercise first.")
         case 'Complete':
-            Exercise_to_complete = values['Exercises'][0]
-            workouts = functions.get_workouts()
-            workouts.remove(Exercise_to_complete)
-            functions.write_workouts(workouts)
-            window['Exercises'].update(values=workouts)
-            window['Exercise'].update(value='')
+            try:
+                Exercise_to_complete = values['Exercises'][0]
+                workouts = functions.get_workouts()
+                workouts.remove(Exercise_to_complete)
+                functions.write_workouts(workouts)
+                window['Exercises'].update(values=workouts)
+                window['Exercise'].update(value='')
+            except IndexError:
+                zx.popup("Please select an exercise first.")
         case 'Exit':
             break
         case 'Exercises':
