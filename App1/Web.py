bd7c7e64bb5c97373171021aaee8c64eedb600f3
Avinash <avkr258@gmail.com>
2024-01-30 23:24:29 -0500

    Implemented Add workout item feature on the web app

--- App1/Web.py
+++ App1/Web.py
@@ -3,10 +3,18 @@ import functions
 
 workouts = functions.get_workouts()
 
+
+def add_workout():
+    workout = zx.session_state['new_workout'] + '\n'
+    workouts.append(workout)
+    functions.write_workouts(workouts)
+
+
 zx.title("My Workout Planner App")
 zx.write("This app is for you to plan out your workouts.")
 
 for workout in workouts:
     zx.checkbox(workout)
 
-zx.text_input(label='', placeholder='Add an exercise...')
+zx.text_input(label='', placeholder='Add an exercise...',
+              on_change=add_workout, key='new_workout')
